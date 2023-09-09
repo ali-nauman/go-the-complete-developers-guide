226ba839c5853c0e88d9a8b44863c3068f79a1f1
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-09-09 13:51:35 +0500

    Functions and return types

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := getNewCard()
 	fmt.Println(card)
+}
+
+func getNewCard() string {
+	return "Five of Diamonds"
 }
