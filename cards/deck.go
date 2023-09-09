cffbbde301f33a8b9af422f87f735218fae20248
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-09-09 22:15:09 +0500

    Writing files

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // A type which is a slice of strings
 type deck []string
@@ -31,4 +35,12 @@ func (d deck) print() {
 // Deals a hand, returning it and the remaining deck
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
