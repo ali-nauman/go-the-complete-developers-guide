04cc410f7ba0c6825f1d61166180fd02e78bb9ef
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-09-11 01:42:03 +0500

    Shuffling a deck

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -57,4 +58,11 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return strings.Split(string(bytes), ",")
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	err := cards.saveToFile("cards.txt")
-
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	cards2 := newDeckFromFile("cards2.txt")
-	cards2.print()
+	cards.shuffle()
+	cards.print()
 }
