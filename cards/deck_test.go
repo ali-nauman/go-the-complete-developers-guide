7a952fcb1edab4e1a1f5c4f7c74c7746bc80bbd8
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-09-13 22:49:08 +0500

    Testing file I/O

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -18,4 +21,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting"
+	os.Remove(fileName)
+
+	d := newDeck()
+	d.saveToFile(fileName)
+
+	d2 := newDeckFromFile(fileName)
+	if len(d2) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d2))
+	}
+
+	os.Remove(fileName)
 }
